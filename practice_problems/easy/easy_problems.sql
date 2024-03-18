7dccebfb06c31ff9d227d6acb6338dedfdd34480
Charles <cw.hanks@proton.me>
2024-03-18 09:57:09 -0700

    linkedin interview question

--- practice_problems/easy/easy_problems.sql
+++ practice_problems/easy/easy_problems.sql
@@ -71,3 +71,46 @@ Pharmacy Analytics (Part 3) [CVS Health SQL Interview Question]
 
 
 """
+
+
+
+
+
+"""
+Data Science Skills [LinkedIn SQL Interview Question]
+
+Given a table of candidates and their skills, you're tasked with finding the candidates best suited for an open Data Science job. You want to find candidates who are proficient in Python, Tableau, and PostgreSQL.
+
+Write a query to list the candidates who possess all of the required skills for the job. Sort the output by candidate ID in ascending order.
+
+Assumption:
+
+There are no duplicates in the candidates table.
+
+candidates Table:
+Column Name	Type
+candidate_id	integer
+skill	varchar
+candidates Example Input:
+candidate_id	skill
+123	Python
+123	Tableau
+123	PostgreSQL
+234	R
+234	PowerBI
+234	SQL Server
+345	Python
+345	Tableau
+Example Output:
+candidate_id
+123
+
+"""
+
+--MY SOLUTION 
+
+select candidate_id from candidates 
+where skill in ('Python', 'Tableau', 'PostgreSQL')
+group by candidate_id
+having count(skill) >= 3
+order by candidate_id;
