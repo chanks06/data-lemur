c74d2fd13ff3b3ea04488bd525bbee6e90a0b356
Charles <cw.hanks@proton.me>
2024-03-22 08:57:55 -0700

    nyt practice problem

--- practice_problems/easy/easy_problems.sql
+++ practice_problems/easy/easy_problems.sql
@@ -228,3 +228,49 @@ select part, assembly_step from parts_assembly
 where finish_date is null; 
 
 
+"""
+Laptop vs. Mobile Viewership [New York Times SQL Interview Question]
+This is the same question as problem #3 in the SQL Chapter of Ace the Data Science Interview!
+
+Assume you're given the table on user viewership categorised by device type where the three types are laptop, tablet, and phone.
+
+Write a query that calculates the total viewership for laptops and mobile devices where mobile is defined as the sum of tablet and phone viewership. Output the total viewership for laptops as laptop_reviews and the total viewership for mobile devices as mobile_views.
+
+Effective 15 April 2023, the solution has been updated with a more concise and easy-to-understand approach.
+viewership Table
+Column Name	Type
+user_id	integer
+device_type	string ('laptop', 'tablet', 'phone')
+view_time	timestamp
+viewership Example Input
+user_id	device_type	view_time
+123	tablet	01/02/2022 00:00:00
+125	laptop	01/07/2022 00:00:00
+128	laptop	02/09/2022 00:00:00
+129	phone	02/09/2022 00:00:00
+145	tablet	02/24/2022 00:00:00
+Example Output
+laptop_views	mobile_views
+2	3
+Explanation
+
+Based on the example input, there are a total of 2 laptop views and 3 mobile views.
+
+The dataset you are querying against may have different input & output - this is just an example!
+
+"""
+
+--I solved this problem by creating a common table expression for each category count (mobile vs. laptop), then select each aggregated column. I could also use the CASE WHEN function.
+
+with cte_laptop as (
+select count(user_id) as laptop_views from viewership
+where device_type in ('laptop')
+),
+
+cte_mobile as (
+select count(user_id) as mobile_views from viewership
+where device_type in ('tablet','phone')
+)
+
+select laptop_views, mobile_views
+from cte_laptop, cte_mobile
